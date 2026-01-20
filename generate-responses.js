d27f83267a2ef9ba3b90f1ba74109b9c19fed891
Chris Gaona <thebeloved88@sbcglobal.net>
2026-01-20 10:12:39 -0800

    Adds additional test reg doc & updates prompt, limit, & number of candidates values

--- generate-responses.js
+++ generate-responses.js
@@ -5,15 +5,18 @@ import OpenAI from 'openai';
 // const QUESTION = "What are the surface wind limits for CDS / Equipment airdops from a C-130 aircraft, and how do these limits adjust for high-velocity drops?";
 // const QUESTION = "What is the purpose of the MAJCOM Civil Engineering Divisions in regards to executing SFO?";
 // const QUESTION = "What are the standard geometric size criteria and minimums for Drop Zone operations, such as for C-130 CDS airdrops up to 600 feet AGL?";
-const QUESTION = "What do I need to know as a FARP surveyor?";
+// const QUESTION = "What do I need to know as a FARP surveyor?";
+// const QUESTION = "What is the minimum runway length for a USAF C-130 aircraft on a Landing Zone at sea level?";
+// const QUESTION = "What are the minimum runway width requirements for a USAF C-130 aircraft on a Landing Zone at sea level, including options for turns specifically from the TSPWG_M_3-260-03_02-19 file?";
+const QUESTION = "What is the glide slope ratio (as part of imaginary surfaces) for approach and departure vertical obstruction clearances for a USAF C-130 aircraft on a Landing Zone at sea level?";
 
 // Specify the search query parameters
 /**
  * It is recommended that you specify a numCandidates number at least 20 times higher than the number of documents to return (limit) to increase accuracy and reduce discrepancies
  */
-const NUM_CANDIDATES = 100; // 100 is 20 times higher than LIMIT of 5
+const NUM_CANDIDATES = 200; // 200 is 20 times higher than LIMIT of 10
 const EXACT = false; // Indicates whether to run ENN (true) or ANN (false) search - If true, numCandidates should be omitted
-const LIMIT = 5;
+const LIMIT = 10;
 
 async function run() {
     try {
@@ -29,7 +32,7 @@ async function run() {
         ).join('\n\n')
 
         // Create a prompt consisting of the question and context to pass to the LLM
-        const prompt = `You are a helpful assistant that answers questions based only on the provided context. Acknowledge limitations when the context provided is incomplete or does not contain relevant information to answer the question. If you need to fill knowledge gaps using information outside of the context, clearly attribute it as such. Always cite the source file, page number, table section / chapter, etc. for where you found the information. If you don't know the answer, kindly apologize, acknowledge the question or comment, and remind the user we can only answer questions about survey related military topics. Don't attempt to fabricate an answer.
+        const prompt = `You are a helpful assistant that answers questions based only on the provided context. Acknowledge limitations when the context provided is incomplete or does not contain relevant information to answer the question. If you need to fill knowledge gaps using information outside of the context, clearly attribute it as such. Always cite the source file, page number, table section / chapter, etc. for where you found the information. If there are multiple sources make sure to mention all sources in your response. If you don't know the answer, kindly apologize, acknowledge the question or comment, and remind the user we can only answer questions about survey related military topics. Don't attempt to fabricate an answer.
             Question: {${QUESTION}}
             Context: {${context}}
         `;
